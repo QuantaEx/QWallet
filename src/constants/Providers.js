86ac0f24f888800a419499ecdf5a9ad9ccec8c56
funnyjerry <wenhu95piao@gmail.com>
2019-05-16 23:22:24 +0800

    update infura api key

--- src/constants/Providers.js
+++ src/constants/Providers.js
@@ -3,7 +3,7 @@ import Config from 'react-native-config';
 const ethers = require('ethers');
 
 const getNetworkProvider = async (network) => {
-  const provider = await new ethers.providers.InfuraProvider(network, Config.INFURA_API_KEY);
+  const provider = await new ethers.providers.InfuraProvider(network, "5360ec7f835141b0bfa74eb029b61425");
   return provider;
 };
 //Where to provide INFURA_API_KEY = "5360ec7f835141b0bfa74eb029b61425"
