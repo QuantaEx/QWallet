356e290e0d8bd7d2327048b0ce81805e63b4bc38
funnyjerry <wenhu95piao@gmail.com>
2019-05-17 16:17:22 +0800

    change theme

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,7 +1,7 @@
 export default{
     color:{
-        background:'#151928', //#f3f4f7
-        text:'#fff',       //#000
-        logoText:'#1f9358' //#1a1f3e
+        background:'#f3f4f7', //#f3f4f7
+        text:'#000',       //#000
+        logoText:'#1a1f3e' //#1a1f3e
     }
 }
